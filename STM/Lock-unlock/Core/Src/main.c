92f77646a9d626d209d4c308d5b6689f98cb12b2
jlurg <jluis_307@hotmail.com>
2025-10-06 21:45:15 -0600

    Test comment main.c

--- STM/Lock-unlock/Core/Src/main.c
+++ STM/Lock-unlock/Core/Src/main.c
@@ -22,7 +22,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-
+// comentario de Luis
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
